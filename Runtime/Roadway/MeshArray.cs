420925cf8592ec231f00a489ccd1ab49f54d03fb
pol <33850434+polcamden@users.noreply.github.com>
2024-11-03 21:37:22 -0600

    carriage way

---
+++ Runtime/Roadway/MeshArray.cs
@@ -0,0 +1,27 @@
+using DecentlyGoodStreetBuilder.Roadway;
+using UnityEngine;
+
+namespace DecentlyGoodStreetBuilder.Roadway
+{
+    public class MeshArray : RoadwayPart, IRoadwayMesh
+    {
+		[SerializeField] public bool collidable;
+		[SerializeField] public Material material;
+
+		[SerializeField] public Vector2[] verticies;
+        [SerializeField] public bool[] sharpVerticies;
+		[SerializeField] public float[] uvs;
+        
+		public bool Collidable
+		{
+			get{ return collidable; }
+		}
+
+		public Material Material
+		{
+			get{ return material; }
+		}
+
+
+	}
+}

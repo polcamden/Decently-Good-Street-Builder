a753f174ff8985e366004b761eb0f9197fe0cdb5
pol <33850434+polcamden@users.noreply.github.com>
2025-05-01 14:18:14 -0500

    movetool changes
    moveTool, now in its own class only selects nodes
    SelectTool, now shown in scene tools selects everything and menu for segment roadway made
    
    Todo:
    -implement roadway library as window
    -addSelected to prevent multiselect on some object
    -selected privated
    -removeSelected to add

---
+++ Editor/MoveTool.cs
@@ -0,0 +1,113 @@
+using DecentlyGoodStreetBuilder.Editor;
+using DecentlyGoodStreetBuilder;
+using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEngine;
+namespace DecentlyGoodStreetBuilder.Editor
+{
+    public class MoveTool : SelectTool
+    {
+        Vector3 center = Vector3.zero;
+
+        public override void OnSelection()
+        {
+            Vector3 c = Vector3.zero;
+            int a = 0;
+
+            //find segments and only select nodes find center for move tool
+            for (int i = 0; i < selected.Count; i++)
+            {
+                if (selected[i].GetType() == typeof(Segment))
+                {
+                    Segment segment = (Segment)selected[i];
+                    selected.Add(segment.GetConnection(0));
+                    selected.Add(segment.GetConnection(1));
+                    selected.RemoveAt(i);
+                    i--;
+                }
+                else
+                {
+                    c += selected[i].Position;
+                    a++;
+                }
+            }
+
+            center = c / a;
+        }
+
+        public override bool DoToSelected()
+        {
+            /*bool selectedOnlySegment = true;
+            for (int i = 0; i < selected.Count; i++)
+            {
+                if (selected[i].GetType() != typeof(Segment))
+                {
+                    selectedOnlySegment = false;
+                    break;
+                }
+            }*/
+
+            if (selected.Count > 0 /*&& !selectedOnlySegment*/)
+            {
+                Vector3 move = Handles.PositionHandle(center, Quaternion.identity) - center;
+
+                foreach (ISelectable selc in selected)
+                {
+                    selc.Position += move;
+                }
+
+                OnSelection();
+
+                if (move != Vector3.zero)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override void PopulateMenu(DropdownMenu menu)
+        {
+            menu.AppendSeparator();
+
+            if (selected.Count > 0)
+            {
+                menu.AppendAction("Destroy", (item) => DestroySelected());
+            }
+
+            if (selected.Count == 2)
+            {
+                menu.AppendAction("Connect", (item) => MeshConnect());
+            }
+            else if (selected.Count >= 3)
+            {
+                //menu.AppendAction("Mesh Connect", (item) => MeshConnect());
+            }
+        }
+
+        public void MeshConnect()
+        {
+            if (selected.Count == 2)
+            {
+                //bad cast just testing
+                Segment seg = ScriptableObject.CreateInstance<Segment>();
+
+                seg.Init((Node)selected[0], (Node)selected[1], ((Node)selected[0]).MyStreetBuilder, null);
+            }
+        }
+
+        public void DestroySelected()
+        {
+            for (int i = 0; i < selected.Count; i++)
+            {
+                if (selected[i].GetType().IsSubclassOf(typeof(StreetElement)))
+                {
+                    DestroyImmediate((StreetElement)selected[i]);
+                }
+            }
+
+            selected.Clear();
+        }
+    }
+}
